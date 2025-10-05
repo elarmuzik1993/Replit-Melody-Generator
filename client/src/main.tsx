60dc30ea8b51df582403ec1bc59957ff678fc3a7
borismiscenco <47517815-borismiscenco@users.noreply.replit.com>
2025-10-05 11:07:23 +0000

    Restored to '9d7f50851eeb8abd1cd6244dc5f41dc1cd6efa8c'
    Replit-Restored-To: 9d7f50851eeb8abd1cd6244dc5f41dc1cd6efa8c

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { Router } from "wouter";
 import App from "./App";
 import "../index.css";
 
-const base = import.meta.env.BASE_URL || "/";
-
-createRoot(document.getElementById("root")!).render(
-  <Router base={base}>
-    <App />
-  </Router>
-);
+createRoot(document.getElementById("root")!).render(<App />);
